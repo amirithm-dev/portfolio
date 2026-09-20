da024b5bda5cea392c24779c1eda06f0a241a17b
amirithm <amirithm01@gmail.com>
2026-09-20 04:34:28 +0330

    install and configure next themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { NextIntlClientProvider } from "next-intl";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,10 +24,13 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col">
         <NextIntlClientProvider>
-          {children}
+          <ThemeProvider attribute={"class"} disableTransitionOnChange={false} enableSystem defaultTheme="system">
+            {children}
+          </ThemeProvider>
         </NextIntlClientProvider>
       </body>
     </html>
